4d039f935c24ad2e55f29f713947eff15981cffc
SrtaEnne <ennenataly@gmail.com>
2019-08-17 17:35:06 -0300

    delete code lines js.js

--- js/js.js
+++ js/js.js
@@ -2,4 +2,13 @@
     $('.modal1').modal();
     $('.sidenav').sidenav();
   });
-      
+  
+  $(document).ready(function(){
+    $('.scrollspy').scrollSpy();
+  });
+
+  var myChart = new Chart(ctx, {
+    type: 'bar',
+    data: data,
+    options: options
+});
